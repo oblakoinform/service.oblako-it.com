4b2964549c9ee368213d845b6dc3a3a865a42948
Александр <25th_kadr@mail.ru>
2019-03-25 10:46:00 +0300

    добавлена главный пустой шаблон

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,7 +6,13 @@ class Main extends CI_Controller {
 
     public function index()
     {
+        $this->load->view("/template/head.php");
 
+
+        $this->load->view("/template/main/index.php");
+
+
+        $this->load->view("/template/foot.php");
     }
 
     public function acts()
---
+++ application/views/template/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Cache-Control" content="private">
+    <meta http-equiv="Cache-Control" content="no-cache">
+	<title>ОБЛАКО СЕРВИС</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap-datepicker.min.css">
+	<script src="/js/jquery-1.9.1.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap-datepicker.min.js"></script>
+    <script src="/js/bootstrap-datepicker.ru.min.js"  charset="UTF-8"></script>
+</head>
+<body>
