7d9f174c90988dd71a123e42f09af30c75871589
Александр <25th_kadr@mail.ru>
2019-03-28 18:54:26 +0300

    до конца не переделал...

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,23 +6,25 @@ class Main extends CI_Controller {
 
     public function index() {
         $this->load->view("/template/head.php",array("title"=>"Акты"));
-        $this->load->view("/main/index.php");
+
         if(isset($_POST['fsrar']) && isset($_POST['month'])){
-        $fsrar = $_POST['fsrar'];
-        $month = $_POST['month'];
-        $month2 = date("m");
-        $month = $month2;
+            $fsrar = $_POST['fsrar'];
+            $month = $_POST['month'];
+            $month2 = date("m");
+            $month = $month2;
+        } else {
+            $fsrar ="";
+            $month ="";
         }
         if(!empty($fsrar)&&($month2)){
-
             $query = $this->db->query("SELECT * FROM egais_acts_position INNER JOIN egais_product ON egais_acts_position.AlcCode = egais_product.AlcCode WHERE egais_Acts_id IN (SELECT id FROM egais_acts WHERE fsrar=? AND month(dt)=?)",array($fsrar,$month2));
-
             $result = $query->result();
-            $this->load->view("/main/index.php",array("result"=>$result));
+        } else {
+            $result=array();
         }
-        else{
-            echo "Выберите месяц и вставьте в поле ФСРАР ИД";
-        }
+        $this->load->view("/main/index.php",array("result"=>$result,"month"=>$month,"fsrar"=>$fsrar));
+
+
        $this->load->view("/template/foot.php");
     }
 
