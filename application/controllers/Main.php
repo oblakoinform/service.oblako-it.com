930553b7df9cda5f08b2323fc0464b49464de8cc
Radik Safiullin <oblakoinform@gmail.ru>
2019-04-12 14:56:26 +0300

    9

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,26 +4,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
 
 
-    public function acts() {
-        $this->load->view("/template/head.php",array("title"=>"Акты по алкоголю"));
+    public function acts()
+    {
+        $this->load->view("/template/head.php", array("title" => "Акты по алкоголю"));
 
-        if(isset($_POST['month'])){
+        if (isset($_POST['month'])) {
             $month = $_POST['month'];
 
         } else {
-            $month =date("m");
+            $month = date("m");
         }
-        $query = $this->db->query("SELECT AlcCode FROM `egais_acts_position` WHERE egais_Acts_id IN(SELECT id FROM egais_acts WHERE MONTH(dt)=?) GROUP BY AlcCode",array($month));
+        $query = $this->db->query("SELECT AlcCode FROM `egais_acts_position` WHERE egais_Acts_id IN(SELECT id FROM egais_acts WHERE MONTH(dt)=?) GROUP BY AlcCode", array($month));
         $result = $query->result();
         $AlcCode = array();
-        foreach($result as $item) {
-            array_push($AlcCode,$item->AlcCode);
+        foreach ($result as $item) {
+            array_push($AlcCode, $item->AlcCode);
         }
-        $query = $this->db->query("SELECT AlcCode FROM egais_product where ProductVCode not  in (263, 520, 500, 510, 262, 261) and AlcCode in ?",array($AlcCode));
+        $query = $this->db->query("SELECT AlcCode FROM egais_product where ProductVCode not  in (263, 520, 500, 510, 262, 261) and AlcCode in ?", array($AlcCode));
         $result = $query->result();
         $pivoAlcCode = array();
-        foreach($result as $item) {
-            array_push($pivoAlcCode ,$item->AlcCode);
+        foreach ($result as $item) {
+            array_push($pivoAlcCode, $item->AlcCode);
         }
 
         $query = $this->db->query("SELECT * FROM `egais_producer` WHERE INN IN('1660287087','1660273045','1656102470','1650332125')  and ClientRegId IN(SELECT DISTINCT fsrar FROM `egais_acts` WHERE MONTH(dt)=? and id in (SELECT egais_acts_id FROM egais_acts_position where AlcCode in ?)) ORDER BY INN DESC",array($month,$pivoAlcCode ));
@@ -63,6 +64,7 @@ class Main extends CI_Controller {
             array_push($pivoAlcCode ,$item->AlcCode);
         }
 
+
         $query = $this->db->query("SELECT * FROM `egais_producer` WHERE INN IN('1660287087','1660273045','1656102470','1650332125')  and ClientRegId IN(SELECT DISTINCT fsrar FROM `egais_acts` WHERE MONTH(dt)=? and id in (SELECT egais_acts_id FROM egais_acts_position where AlcCode in ?)) ORDER BY INN DESC",array($month,$pivoAlcCode ));
         $producer = $query->result();
 
@@ -70,6 +72,7 @@ class Main extends CI_Controller {
         foreach($producer as $item) {
             $Aproducer[$item->ClientRegId] = $item;
         }
+
 
         $this->load->view("/main/acts_pivo.php",array("Aproducer"=>$Aproducer,"month"=>$month));
         $this->load->view("/template/foot.php");
@@ -117,6 +120,11 @@ class Main extends CI_Controller {
 
         $month = $_GET['month'];
         $ClientRegId = $_GET['ClientRegId'];
+        if (isset($_GET['showpivo'])) {
+            $showpivo = $_GET['showpivo'];
+        } else {
+            $showpivo =null;
+        }
 
         $query = $this->db->query("SELECT AlcCode, sum(Quantity) as Quantity FROM `egais_acts_position` WHERE egais_Acts_id IN(SELECT id FROM egais_acts WHERE fsrar=? AND MONTH(dt)=? AND type IN('ActWriteOffShop_v2','ActWriteOff_v3')) GROUP BY AlcCode",array($ClientRegId,$month));
         $actWrite = $query->result();
@@ -144,7 +152,7 @@ class Main extends CI_Controller {
             $Aproduct[$item2->AlcCode] = $item2;
         }
 
-        $this->load->view("/main/getdata.php",array("actWrite"=>$actWrite,"actCharge"=>$actCharge,"ClientRegId"=>$ClientRegId,"month"=>$month,"Aproduct"=>$Aproduct));
+        $this->load->view("/main/getdata.php",array("showpivo"=>$showpivo,"actWrite"=>$actWrite,"actCharge"=>$actCharge,"ClientRegId"=>$ClientRegId,"month"=>$month,"Aproduct"=>$Aproduct));
 
 
     }
