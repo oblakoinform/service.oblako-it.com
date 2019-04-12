6fadf75ae05a6235b4a2b8f01af2974d863b3593
Александр <25th_kadr@mail.ru>
2019-04-12 14:37:04 +0300

    обновление

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,26 +4,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
 
 
-    public function acts() {
-        $this->load->view("/template/head.php",array("title"=>"Акты по алкоголю"));
+    public function acts()
+    {
+        $this->load->view("/template/head.php", array("title" => "Акты по алкоголю"));
 
-        if(isset($_POST['month'])){
+        if (isset($_POST['month'])) {
             $month = $_POST['month'];
 
         } else {
-            $month =date("m");
+            $month = date("m");
         }
-        $query = $this->db->query("SELECT AlcCode FROM `egais_acts_position` WHERE egais_Acts_id IN(SELECT id FROM egais_acts WHERE MONTH(dt)=?) GROUP BY AlcCode",array($month));
+        $query = $this->db->query("SELECT AlcCode FROM `egais_acts_position` WHERE egais_Acts_id IN(SELECT id FROM egais_acts WHERE MONTH(dt)=?) GROUP BY AlcCode", array($month));
         $result = $query->result();
         $AlcCode = array();
-        foreach($result as $item) {
-            array_push($AlcCode,$item->AlcCode);
+        foreach ($result as $item) {
+            array_push($AlcCode, $item->AlcCode);
         }
-        $query = $this->db->query("SELECT AlcCode FROM egais_product where ProductVCode not  in (263, 520, 500, 510, 262, 261) and AlcCode in ?",array($AlcCode));
+        $query = $this->db->query("SELECT AlcCode FROM egais_product where ProductVCode not  in (263, 520, 500, 510, 262, 261) and AlcCode in ?", array($AlcCode));
         $result = $query->result();
         $pivoAlcCode = array();
-        foreach($result as $item) {
-            array_push($pivoAlcCode ,$item->AlcCode);
+        foreach ($result as $item) {
+            array_push($pivoAlcCode, $item->AlcCode);
         }
 
         $query = $this->db->query("SELECT * FROM `egais_producer` WHERE INN IN('1660287087','1660273045','1656102470','1650332125')  and ClientRegId IN(SELECT DISTINCT fsrar FROM `egais_acts` WHERE MONTH(dt)=? and id in (SELECT egais_acts_id FROM egais_acts_position where AlcCode in ?)) ORDER BY INN DESC",array($month,$pivoAlcCode ));
